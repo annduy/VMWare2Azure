9e1338f973ed1057ad036fcd77194d4024ceb40f
Annduy <annduy@gmail.com>
2023-10-03 13:45:19 +0700

    20231003-1345

--- VMWare2Azure.py
+++ VMWare2Azure.py
@@ -4,6 +4,7 @@ import time
 import threading
 import sys
 import queue
+from datetime import datetime
 
 
 # Replace with your values###########
@@ -11,10 +12,10 @@ powershell = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
 qemu_img = 'D:\\qemu-img\\qemu-img.exe'
 #=== VMWare VM info
 vmdk_path ='G:\\VMWare-VMs\\UbuntuServer20046'
-vmdk_hdds = ['UbuntuServer20046.vmdk', 'UbuntuServer20046-0.vmdk'] # the bootable disk must be at 1st in the list!!!!
+vmdk_hdds = ['UbuntuServer20046.vmdk','UbuntuServer20046-0.vmdk'] # the bootable disk must be at 1st in the list!!!!
 
 #=== Hyper-V VM info
-vhd_path= "G:\\HyperV-VMs\\UbuntuServer20046"
+vhd_path= "D:\\HyperV-VMs\\UbuntuServer20046"
 vhd_hdds = []
 #==== AZ info
 az_vm_name = ''
@@ -24,7 +25,7 @@ az_bootable_disk = ''
 
 def obtain_vm_name(vmdk_path):
     tokens = vmdk_path.split('\\')
-    vm_name = tokens[-1]
+    vm_name = tokens[-1] + '-vm'
     return vm_name
 
 def genarate_az_bootable_disk_name(bootable_vmdk_hdd):
@@ -40,6 +41,11 @@ def generate_vhd_hdds_and_az_hdds(vmdk_hdds):
     return vhd_hdds, az_hdds
  
 
+def current_date_time_string():
+    current_datetime = datetime.now()
+    current_datetime_string = current_datetime.strftime("%d/%m/%Y - %H:%M:%S")
+    return current_datetime_string
+
 def powershell_in_subprocess(command, queue):
     result = subprocess.run([powershell,'-command',command],stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
     queue.put(result)
@@ -51,9 +57,9 @@ def run_ps_command_in_thread(command):
     task_progress(task)
     result = result_queue.get()
     if result.stdout != '':
-        print("Result out from thread:", result.stdout)
+        print("Result out from thread: ", result.stdout)
     if result.stderr != '':
-        print("Result error from thread:", result.stderr)  
+        print("Warning(s)/Error(s) from thread: ", result.stderr)  
 
 def task_progress(task):
     while task.is_alive():
@@ -71,13 +77,13 @@ def convert_vmdks_2_vhds(vmdk_path,vmdk_hdds,vhd_path):
         for vmdk_hdd in vmdk_hdds:
             vhd_hdd = vmdk_hdd.replace('vmdk','vhd')
             convert_command = f'{qemu_img} convert -f vmdk -O vpc -o subformat=fixed {vmdk_path}\{vmdk_hdd} {vhd_path}\{vhd_hdd}'
-            print(f"converting file: {vmdk_path}\{vmdk_hdd}"  )
+            print(current_date_time_string() + " : " + f"converting file: {vmdk_path}\{vmdk_hdd}"  )
             run_ps_command_in_thread(convert_command)
             hdd_len_in_bytes = os.path.getsize(f'{vhd_path}\{vhd_hdd}')
             if (hdd_len_in_bytes % (1024*1024)) != 0: 
                 hdd_len_in_meg = ((hdd_len_in_bytes // (1024*1024)) + 1)
                 resize_command = f"Resize-VHD {vhd_path}\{vhd_hdd} -SizeBytes {hdd_len_in_meg}MB"
-                print(f"Resizing file: {vhd_path}\{vhd_hdd}"  )
+                print(current_date_time_string() + " : " + f"Resizing file: {vhd_path}\{vhd_hdd}"  )
                 run_ps_command_in_thread(resize_command)
     except:
         print("Encountered an error during convert VMDKs to VHDs.")
@@ -88,7 +94,7 @@ def upload_vhds_2_az_storage(vhd_path,vhd_hdds):
     try:
         for vhd in vhd_hdds:
             upload_command = f"az storage blob upload --account-name {az_storage} --account-key {az_key} --container-name {az_container}  --name {vhd} --file {vhd_path}\{vhd} --overwrite true"
-            print(f"Uploading file {vhd_path}\{vhd} to Azure Storage")
+            print(current_date_time_string() + " : " + f"Uploading file {vhd_path}\{vhd} to Azure Storage")
             run_ps_command_in_thread(upload_command)
     except:
         print("Encountered an error during uploading VHDs to Azure storage.")
@@ -97,8 +103,9 @@ def upload_vhds_2_az_storage(vhd_path,vhd_hdds):
 def create_az_disk_from_uploaded_vhd(vhd,os_type='Linux',sku='Standard_LRS'):
     global az_resourcegroup, az_storage,az_container
     az_disk = vhd.strip('.vhd') + '-dsk'
-    create_az_disk_cmd = f"az disk create -g {az_resourcegroup} -n {az_disk} --os-type {os_type} --sku {sku} --source https://{az_storage}.blob.core.windows.net/{az_container}/{vhd}"
-    print(f"Creating AZDisk from {vhd}")
+    #create_az_disk_cmd = f"az disk create -g {az_resourcegroup} -n {az_disk} --os-type {os_type} --sku {sku} --source https://{az_storage}.blob.core.windows.net/{az_container}/{vhd}"
+    create_az_disk_cmd = f"az disk create -g {az_resourcegroup} -n {az_disk} --sku {sku} --source https://{az_storage}.blob.core.windows.net/{az_container}/{vhd}"
+    print(current_date_time_string() + " : " + f"Creating AZDisk from {vhd}")
     run_ps_command_in_thread(create_az_disk_cmd)
 
 def create_az_disks_from_all_uploaded_vhds(vhd_hdds):
@@ -108,26 +115,26 @@ def create_az_disks_from_all_uploaded_vhds(vhd_hdds):
 
 def create_az_vm_from_bootable_az_disk(vm_name,az_disk, os_type='Linux', size='Standard_B1ls'):
     global az_resourcegroup
-    creat_az_vm_cmd = f"az vm create -g {az_resourcegroup} -n {vm_name} --attach-os-disk {az_disk} --os-type {os_type} --size {size}"
-    print(f"Creating AZ VM from {az_disk}")
+    creat_az_vm_cmd = f"az vm create -g {az_resourcegroup} -n {vm_name} --attach-os-disk {az_disk} --os-type {os_type} --size {size} --public-ip-sku Standard"
+    print(current_date_time_string() + " : " + f"Creating AZ VM from {az_disk}")
     run_ps_command_in_thread(creat_az_vm_cmd)
 
 def attach_non_bootable_disk_to_az_vm(vm_name, az_disk, sku='Standard_LRS'):
     global az_resourcegroup
     attach_disk_cmd = f"az vm disk attach --resource-group {az_resourcegroup} --vm-name {vm_name} --name {az_disk} --sku {sku}"
-    print(f"Attaching {az_disk} to {vm_name}")
+    print(current_date_time_string() + " : " + f"Attaching {az_disk} to {vm_name}")
     run_ps_command_in_thread(attach_disk_cmd)
 
 
 def delete_uploaded_vhds():
     global az_container, az_storage, az_key
     az_delete_vhds_command = f'az storage blob delete-batch --account-key {az_key} -s {az_container} --account-name {az_storage} --pattern *.vhd'
-    print("Delelting all uploaded VHD(s).")
+    print(current_date_time_string() + " : " + "Delelting all uploaded VHD(s).")
     run_ps_command_in_thread(az_delete_vhds_command)
 
 def delete_local_vhds(vhd_path):
     delete_local_vhds_command = f'Remove-Item –path {vhd_path} –recurse'
-    print("Deleting all generated VHD(s).")
+    print(current_date_time_string() + " : " + "Deleting all generated VHD(s).")
     run_ps_command_in_thread(delete_local_vhds_command)
 
 
@@ -147,7 +154,7 @@ vhd_hdds, az_hdds = generate_vhd_hdds_and_az_hdds(vmdk_hdds)
 az_vm_name = obtain_vm_name(vmdk_path)
 az_bootable_disk = genarate_az_bootable_disk_name(vmdk_hdds[0])
 
-#convert_vmdks_2_vhds(vmdk_path,vmdk_hdds,vhd_path)
+convert_vmdks_2_vhds(vmdk_path,vmdk_hdds,vhd_path)
 
 upload_vhds_2_az_storage(vhd_path,vhd_hdds)
 
--- testpsinpython.py
+++ testpsinpython.py
@@ -1,19 +1,9 @@
 import subprocess
 
-cmd = 'az storage blob upload --account-name annduystorage --account-key G3Jc6uGXicw7QMZDIJYDZC/aeNFUzzpznVsNaFUd9wBdrjDCA2flwzCAXsP/CRYrPX63R6bguoZJ+AStqd/Fgw== --container-name uploadstorage  --name UbuntuServer20046.hdd --file G:\\HyperV-VMs\\UbuntuServer20046\\UbuntuServer20046.hdd'
+cmd = 'az vm create -g SDWan -n UbuntuServer20046-vm --attach-os-disk UbuntuServer20046-dsk --size Standard_B1ls --os-type Linux --public-ip-sku Standard'
 
 result = subprocess.run(['powershell', '-command', cmd], stderr=subprocess.PIPE, text=True)
 
 # Print the error output
 print(result.stderr)
 
-# def obtain_vm_name(vmdk_path):
-#     tokens = vmdk_path.split('\\')
-#     vm_name = tokens[-1]
-#     print(vm_name)
-#     return vm_name
-
-
-
-# az_vm_name = obtain_vm_name('G:\\VMWare-VMs\\UbuntuServer20046')
-# print(az_vm_name)
